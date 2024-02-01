5834162a68aadaa60fca4ccb2dca61438456c341
SebastianL2 <sebas16cely@gmail.com>
2024-02-01 13:54:14 -0500

    setup created

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+    </div>
+
+</template>
+
+
